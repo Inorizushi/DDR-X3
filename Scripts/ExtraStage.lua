3ba8d6a761f76aefbac8a4ab858660a1903aaf80
Flameshadowxeroshin <flameshadowxeroshin@gmail.com>
2017-03-13 15:56:37 -0500

    one last diddle

--- Scripts/ExtraStage.lua
+++ Scripts/ExtraStage.lua
@@ -7,11 +7,12 @@ local gradeAwards = {
 setmetatable(gradeAwards, __index=function() return 1 end)
 
 function ExtraStage.GetStarsFromPSS(pss)
+	local grade = pss:GetGrade()
 	if grade == 'Grade_Failed' then
 		return 0
 	end
 	local possible_stars = 1 --for clear
-	possible_stars = possible_stars + gradeAwards[pss:GetGrade()]
+	possible_stars = possible_stars + gradeAwards[grade]
 	possible_stars = possible_stars + pss:FullCombo() and 1 or 0
 	return math.min(possible_stars, 3)
 end
